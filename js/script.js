749d7d9fa9f0f5bad33f322670f64b27e06cf96d
hosam8081 <hosamsalah555@yahoo.com>
2021-02-25 06:29:15 +0200

    quiz app

---
+++ js/script.js
@@ -0,0 +1,548 @@
+var api = [
+    {
+        "title": "was defined as describing and analyzing the distribution of buldings",
+        "answer_1": "human geography",
+        "answer_2": "urbn geography",
+        "answer_3": "population geography",
+        "answer_4": "economic geography",
+        "right":"answer_2",
+        "right_answer": "urbn geography"
+    },
+
+
+    {
+        "title": ".... looks at the area inhabited by human on Earth",
+        "answer_1": "human geography",
+        "answer_2": "urbn geography",
+        "answer_3": "population geography",
+        "answer_4": "economic geography",
+        "right":"answer_2",
+        "right_answer": "urbn geography"
+    },
+    {
+        "title": ".... is often referred to as culture",
+        "answer_1": "human geography",
+        "answer_2": "urbn geography",
+        "answer_3": "population geography",
+        "answer_4": "economic geography",
+        "right":"answer_1",
+        "right_answer": "human geography"
+    },
+    {
+        "title": "social studies is often taught to .... studies",
+        "answer_1": "young",
+        "answer_2": "old",
+        "answer_3": "adult",
+        "answer_4": "all of above",
+        "right":"answer_1",
+        "right_answer": "young"
+    },
+    {
+        "title": "... are areas where cities and towns are located so close together",
+        "answer_1": "central city ",
+        "answer_2": "physical city",
+        "answer_3": "metropolitan city",
+        "answer_4": "urbanized city",
+        "right":"answer_4",
+        "right_answer": "urbanized city"
+    },
+    {
+        "title": "louis wirth defined city as a .... settlement that has three characteries",
+        "answer_1": "temporary",
+        "answer_2": "permanent",
+        "answer_3": "a , b",
+        "answer_4": "All of above",
+        "right":"answer_2",
+        "right_answer": "permanent"
+    },
+    {
+        "title": "city is a permanent settlement that has three characeries is ....",
+        "answer_1": "large size",
+        "answer_2": "high density",
+        "answer_3": "social heterogeneity",
+        "answer_4": "all of above",
+        "right":"answer_4",
+        "right_answer": "all of above"
+    },
+    {
+        "title": "... is the largest city in a state",
+        "answer_1": "primate city",
+        "answer_2": "mega city",
+        "answer_3": "mercantile city",
+        "answer_4": "manufacturing city",
+        "right":"answer_1",
+        "right_answer": "primate city"
+    },
+    {
+        "title": "... is a city where trade is central to its design",
+        "answer_1": "primate city",
+        "answer_2": "mega city",
+        "answer_3": "mercantile city",
+        "answer_4": "manufacturing city",
+        "right":"answer_3",
+        "right_answer": "mercantile city"
+    },
+    {
+        "title": "... is a city formed in the indusrial revolution",
+        "answer_1": "primate city",
+        "answer_2": "mega city",
+        "answer_3": "mercantile city",
+        "answer_4": "manufacturing city",
+        "right":"answer_4",
+        "right_answer": "manufacturing city"
+    },
+    {
+        "title": "... is a city whose socioeconomics impact the entire world",
+        "answer_1": "primate city",
+        "answer_2": "world city",
+        "answer_3": "urbanized city",
+        "answer_4": "manufacturing city",
+        "right":"answer_2",
+        "right_answer": "world city"
+    },
+    {
+        "title": "... is a city whose population is greater than 10 million people",
+        "answer_1": "primate city",
+        "answer_2": "mega city",
+        "answer_3": "urbanized city",
+        "answer_4": "manufacturing city",
+        "right":"answer_2",
+        "right_answer": "mega city"
+    },
+    {
+        "title": "mega city is a city whose population is greater than ... million people",
+        "answer_1": "10",
+        "answer_2": "15",
+        "answer_3": "10",
+        "answer_4": "9",
+        "right":"answer_1",
+        "right_answer": "10"
+    },
+    {
+        "title": "Which of the types of cities ?",
+        "answer_1": "global function cities",
+        "answer_2": "multi-functional cities",
+        "answer_3": "transportion center",
+        "answer_4": "b, c",
+        "right":"answer_4",
+        "right_answer": "b, c"
+    },
+    {
+        "title": "... a place where a major routes converge",
+        "answer_1": "special function cities",
+        "answer_2": "multi-functional cities",
+        "answer_3": "transportion center",
+        "answer_4": "all of above",
+        "right":"answer_3",
+        "right_answer": "transportion center"
+    },
+    {
+        "title": "... do a number of activites based on the needs of the population",
+        "answer_1": "special function cities",
+        "answer_2": "multi-functional cities",
+        "answer_3": "transportion center",
+        "answer_4": "all of above",
+        "right":"answer_2",
+        "right_answer": "multi-functional cities"
+    },
+    {
+        "title": "multi-functional cities do a number of activites based on the needs of the ...",
+        "answer_1": "population",
+        "answer_2": "worker",
+        "answer_3": "mining",
+        "answer_4": "all of above",
+        "right":"answer_1",
+        "right_answer": "population"
+    },
+    {
+        "title": "Which of the types of jobs in a city ?",
+        "answer_1": "basic sector jobs",
+        "answer_2": "non basic sector jobs",
+        "answer_3": "none of above",
+        "answer_4": "a, b",
+        "right":"answer_4",
+        "right_answer": "a, b"
+    },
+    {
+        "title": "... is production of goods and services for sale outside the city",
+        "answer_1": "basic sector jobs",
+        "answer_2": "non basic sector jobs",
+        "answer_3": "none of above",
+        "answer_4": "a, b",
+        "right":"answer_1",
+        "right_answer": "basic sector jobs"
+    },
+    {
+        "title": "... is goods and services for the inside city itself",
+        "answer_1": "basic sector jobs",
+        "answer_2": "non basic sector jobs",
+        "answer_3": "none of above",
+        "answer_4": "a, b",
+        "right":"answer_2",
+        "right_answer": "non basic sector jobs"
+    },
+    {
+        "title": "... area around a city that is affected by it",
+        "answer_1": "primate city",
+        "answer_2": "urbanized zone",
+        "answer_3": "urban influence zone",
+        "answer_4": "a, b",
+        "right":"answer_3",
+        "right_answer": "urban influence zone"
+    },
+    {
+        "title": "... is the study of the location, distrbution and spatial organization of economic activites",
+        "answer_1": "human geography",
+        "answer_2": "urbn geography",
+        "answer_3": "population geography",
+        "answer_4": "economic geography",
+        "right":"answer_4",
+        "right_answer": "economic geography"
+    },
+    {
+        "title": "... studies the impact of economic activites on the landscape",
+        "answer_1": "human geography",
+        "answer_2": "urbn geography",
+        "answer_3": "population geography",
+        "answer_4": "economic geography",
+        "right":"answer_4",
+        "right_answer": "economic geography"
+    },
+    {
+        "title": "... focuses on building theories about spatial arrangement and distribution of economic activites",
+        "answer_1": "theoretical economic geography",
+        "answer_2": "regional economic geography",
+        "answer_3": "historical economic geography",
+        "answer_4": "critical economic geography",
+        "right":"answer_1",
+        "right_answer": "theoretical economic geography"
+    },
+    {
+        "title": "... examins the economic conditions of particular regions or countries of the world",
+        "answer_1": "theoretical economic geography",
+        "answer_2": "regional economic geography",
+        "answer_3": "historical economic geography",
+        "answer_4": "critical economic geography",
+        "right":"answer_2",
+        "right_answer": "regional economic geography"
+    },
+    {
+        "title": "... is the process by which economic activites evolved ",
+        "answer_1": "modernization modal",
+        "answer_2": "agriculture",
+        "answer_3": "depency theory",
+        "answer_4": "industrialization",
+        "right":"answer_4",
+        "right_answer": "industrialization"
+    },
+    {
+        "title": "economic activites evolved from producing .... goods to .... goods ",
+        "answer_1": "mass-produce/primary",
+        "answer_2": "primary/mass-produce",
+        "answer_3": "primary/secondary",
+        "answer_4": "none of the above",
+        "right":"answer_2",
+        "right_answer": "primary/mass-produce"
+    },
+    {
+        "title": "... have not developed idustry",
+        "answer_1": "less developed countries(LDC)",
+        "answer_2": "modern developed countries(MDC)",
+        "answer_3": "gross domestic product",
+        "answer_4": "none of the above",
+        "right":"answer_1",
+        "right_answer": "less developed countries(LDC)"
+    },
+    {
+        "title": "... the value of total output of goods and services produced in a year",
+        "answer_1": "modernization model",
+        "answer_2": "dependency theory",
+        "answer_3": "gross domestic product",
+        "answer_4": "none of the above",
+        "right":"answer_3",
+        "right_answer": "gross domestic product"
+    },
+    {
+        "title": "... says that industrial revolution was spurred by a conmbination of prosperity and inventions",
+        "answer_1": "modernization model",
+        "answer_2": "dependency theory",
+        "answer_3": "gross domestic product",
+        "answer_4": "none of the above",
+        "right":"answer_1",
+        "right_answer": "modernization model"
+    },
+    {
+        "title": "... says that the economic development of many countries is blocked by industrialized nations that (expolit) them",
+        "answer_1": "modernization model",
+        "answer_2": "dependency theory",
+        "answer_3": "gross domestic product",
+        "answer_4": "none of the above",
+        "right":"answer_2",
+        "right_answer": "dependency theory"
+    },
+    {
+        "title": "... the process of technological change that start in 1700s",
+        "answer_1": "modernization model",
+        "answer_2": "dependency theory",
+        "answer_3": "the location theory",
+        "answer_4": "industrial revolution",
+        "right":"answer_4",
+        "right_answer": "industrial revolution"
+    },
+    {
+        "title": "... explains the locational pattern of economic activites by identifying factors that influence this pattern",
+        "answer_1": "modernization model",
+        "answer_2": "dependency theory",
+        "answer_3": "the location theory",
+        "answer_4": "industrial revolution",
+        "right":"answer_3",
+        "right_answer": "the location theory"
+    },
+    {
+        "title": "... develops around natural resources",
+        "answer_1": "primary industry",
+        "answer_2": "secondary industry",
+        "answer_3": "the location theory",
+        "answer_4": "none of above",
+        "right":"answer_1",
+        "right_answer": "primary industry"
+    },
+    {
+        "title": "... develops as transporation improves",
+        "answer_1": "primary industry",
+        "answer_2": "secondary industry",
+        "answer_3": "the location theory",
+        "answer_4": "none of above",
+        "right":"answer_2",
+        "right_answer": "secondary industry"
+    },
+    {
+        "title": "... services that support economic activites. it provides transporation and education",
+        "answer_1": "infrastructure",
+        "answer_2": "structure",
+        "answer_3": "industialization",
+        "answer_4": "none of above",
+        "right":"answer_1",
+        "right_answer": "infrastructure"
+    },
+    {
+        "title": "... is the study of the relationship between people, and territory",
+        "answer_1": "political geography",
+        "answer_2": "state",
+        "answer_3": "nation",
+        "answer_4": "none of above",
+        "right":"answer_1",
+        "right_answer": "political"
+    },
+    {
+        "title": "A state is a political unit with",
+        "answer_1": "a permanent population",
+        "answer_2": "sovereign territory",
+        "answer_3": "legitmacy by other state",
+        "answer_4": "all of above",
+        "right":"answer_4",
+        "right_answer": "all of above"
+    },
+    {
+        "title": ".... is a state that include more than one nation within its borders",
+        "answer_1": "multinational state",
+        "answer_2": "buffer state",
+        "answer_3": "staellite state",
+        "answer_4": "all of above",
+        "right":"answer_1",
+        "right_answer": "multinational state"
+    },
+    {
+        "title": ".... is a state located between two larger conflicting countries",
+        "answer_1": "multinational state",
+        "answer_2": "buffer state",
+        "answer_3": "staellite state",
+        "answer_4": "all of above",
+        "right":"answer_2",
+        "right_answer": "buffer state"
+    },
+    {
+        "title": ".... is a state is formally independent but under heavy military",
+        "answer_1": "multinational state",
+        "answer_2": "buffer state",
+        "answer_3": "staellite state",
+        "answer_4": "all of above",
+        "right":"answer_2",
+        "right_answer": "buffer state"
+    },
+    
+
+
+    {
+        "title": "... to make it little or small as possible",
+        "answer_1": "minimizing",
+        "answer_2": "maxmizing",
+        "answer_3": "expand",
+        "answer_4": "a, d",
+        "right":"answer_1",
+        "right_answer": "minimizing"
+    },
+    {
+        "title": "... steps to do a thing",
+        "answer_1": "affordable",
+        "answer_2": "shift",
+        "answer_3": "procedure",
+        "answer_4": "a, d",
+        "right":"answer_3",
+        "right_answer": "procedure"
+    },
+    {
+        "title": "... profits or income or gains as result of making bussiness",
+        "answer_1": "affordable",
+        "answer_2": "shift",
+        "answer_3": "procedure",
+        "answer_4": "revenues",
+        "right":"answer_4",
+        "right_answer": "revenues"
+    },
+    {
+        "title": "... middlemen, persons link buyres with sellers",
+        "answer_1": "manger",
+        "answer_2": "supervisor",
+        "answer_3": "intermediaries",
+        "answer_4": "none of above",
+        "right":"answer_3",
+        "right_answer": "intermediaries"
+    },
+    {
+        "title": "... checking what is in stock",
+        "answer_1": "revenues",
+        "answer_2": "inventory",
+        "answer_3": "procedure",
+        "answer_4": "none of above",
+        "right":"answer_2",
+        "right_answer": "inventory"
+    },
+    {
+        "title": "affordable means it can be ....",
+        "answer_1": "born",
+        "answer_2": "borne",
+        "answer_3": "bear",
+        "answer_4": "a, d",
+        "right":"answer_2",
+        "right_answer": "borne"
+    },
+    {
+        "title": "request means ....",
+        "answer_1": "demand",
+        "answer_2": "order",
+        "answer_3": "expand",
+        "answer_4": "a, b",
+        "right":"answer_2",
+        "right_answer": "order"
+    },
+    {
+        "title": "encrypt means ....",
+        "answer_1": "discode",
+        "answer_2": "incode",
+        "answer_3": "decode",
+        "answer_4": "a, b",
+        "right":"answer_2",
+        "right_answer": "incode"
+    },
+    {
+        "title": "ecommerce start with product .... and ends with customer .... ",
+        "answer_1": "information/service",
+        "answer_2": "service/information",
+        "answer_3": "order/payment",
+        "answer_4": "a, b",
+        "right":"answer_1",
+        "right_answer": "1"
+    },
+
+]
+
+var questionArea = document.querySelector('#que');
+var li  = document.querySelectorAll('li');
+var btn = document.querySelector('.btn');
+var current = 0;
+var score = 0;
+document.addEventListener('DOMContentLoaded', function(){
+    showdata()
+})
+
+function showdata() {
+
+    var html = `<h1>qustion <span>${current + 1}</span>/${api.length}</h1>
+    <h2> ${api[current].title} </h2>
+    
+    <ul>
+        <li class="ans">
+            <span>${api[current].answer_1}</span>
+            <input class="box" type="radio" name="answers" id="answer_1" value="${api[current].answer_1}" >
+        </li>
+        <li class="ans">
+            ${api[current].answer_2}
+            <input class="box" type="radio" name="answers" id="answer_2" value="${api[current].answer_2}">
+        </li>
+        <li class="ans">
+            ${api[current].answer_3}
+            <input class="box" type="radio" name="answers" id="answer_3" value="${api[current].answer_3}" >
+        </li>
+        <li class="ans">
+            ${api[current].answer_4}
+            <input class="box" type="radio" name="answers" id="answer_4" value="${api[current].answer_4}">
+        </li>
+
+        <button class="btn">next</button>
+    </ul>`
+
+    questionArea.innerHTML = html;
+    console.log(api[0]);
+
+    document.querySelectorAll(".box").forEach((ele, index) => {
+        if (index == 0) {
+            ele.checked = 'true'
+        }
+    })
+
+}
+
+document.addEventListener('click', function(e){
+    
+    // CHECK answers
+    if (e.target.classList.contains("btn")) {
+        // loop for all input
+        document.querySelectorAll(".box").forEach((ele) => {
+            if (ele.checked) {
+                if (ele.value === api[current].right_answer) {
+                    score++;
+                   document.querySelector(`#${api[current].right}`).parentElement.classList.add("right-answer");
+                } else {
+
+                    document.querySelector(`#${api[current].right}`).parentElement.classList.add("right-answer");
+                    console.log(ele.parentElement.classList.add("warning-answer"))
+                }
+            }
+        })
+    }
+
+    // loop for answer box
+    if (e.target.classList.contains('ans')) {
+       e.target.querySelector('input').checked = "checked"
+    }
+
+    if (e.target.classList.contains('btn')) {
+        if (current >= api.length -1) {
+            questionArea.innerHTML = `<div class="alert alert-primary">
+            you get ${score} of ${api.length}
+        </div>`
+        } else {
+            e.target.classList.add('pointer');
+            setTimeout(function() {
+                current ++; 
+                showdata();
+            }, 500)
+        }
+    } 
+
+})
+
+
+
+console.log(api)
